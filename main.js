03e8353b0bf144132cbc868e746f46ef80d1b6fb
APolis-WebAPs <105502294+APolis-WebAPs@users.noreply.github.com>
2022-05-13 16:55:59 +0300

    Add Initial files

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Functions
+//function hello() {
+//console.log("Hello world!")
+//}
+
+//let sum = (a, b) => {
+//    return a + b;
+//}
+//console.log(sum(5, 20));
+
+for(let x=1; x<10; x++) {
+
+      for(let y=1; y<10; y++){
+     console.log(x, "X", y, "=", x*y)
+     }
+}
